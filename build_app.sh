18556d51dce96b492574ef1b428f7d18829c79b6
MiriamMendelson <miriam.mendelson@nextsilicon.com>
2022-12-11 12:50:13 +0200

    added -lrt flag

--- build_app.sh
+++ build_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-gcc -fsanitize=address -fsanitize=undefined -Wall -Wextra -Werror -o ./out_bin/app ./*.c -L./out_bin -lFTN -g
+gcc -fsanitize=address -fsanitize=undefined -Wall -Wextra -Werror -o ./out_bin/app ./*.c -L./out_bin -lFTN -g -lrt
